4fe132eb4f4a9333f3aa62d7a9d50b3c6edceea6
Sriram Yagnaraman <sriram.yagnaraman@est.tech>
2023-10-09 11:12:03 +0200

    Remove in-memory store and use /etc/cni/net.d/*.conflist as store

--- pkg/server/networkattachment.go
+++ pkg/server/networkattachment.go
@@ -5,9 +5,18 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+
+	network "github.com/sriramy/vf-operator/pkg/api/v1/gen/network"
 )
 
-func BuildNetworkAttachmentConfig(cniConfig map[string]interface{}, pciAddress string) (map[string]interface{}, error) {
+func AddNameToNetworkAttachment(cniConfig map[string]interface{}, name string, resourceName string) (map[string]interface{}, error) {
+	cniConfig["name"] = name
+	cniConfig["resourceName"] = resourceName
+	return cniConfig, nil
+}
+
+func AddDeviceIDToNetworkAttachment(cniConfig map[string]interface{}, pciAddress string) (map[string]interface{}, error) {
 	pList, ok := cniConfig["plugins"]
 	if !ok {
 		if _, ok := cniConfig["type"]; ok {
@@ -15,18 +24,18 @@ func BuildNetworkAttachmentConfig(cniConfig map[string]interface{}, pciAddress s
 			cniConfig["pciBusID"] = pciAddress
 			return cniConfig, nil
 		}
-		return cniConfig, fmt.Errorf("BuildNetworkAttachmentConfig: plugins nor type key found")
+		return cniConfig, fmt.Errorf("AddDeviceIDToNetworkAttachment: plugins nor type key found")
 	}
 
 	pMap, ok := pList.([]interface{})
 	if !ok {
-		return cniConfig, fmt.Errorf("BuildNetworkAttachmentConfig: unable to typecast plugin list")
+		return cniConfig, fmt.Errorf("AddDeviceIDToNetworkAttachment: unable to typecast plugin list")
 	}
 
 	for idx, plugin := range pMap {
 		currentPlugin, ok := plugin.(map[string]interface{})
 		if !ok {
-			return cniConfig, fmt.Errorf("BuildNetworkAttachmentConfig: unable to typecast plugin #%d", idx)
+			return cniConfig, fmt.Errorf("AddDeviceIDToNetworkAttachment: unable to typecast plugin #%d", idx)
 		}
 		currentPlugin["deviceID"] = pciAddress
 		currentPlugin["pciBusID"] = pciAddress
@@ -42,7 +51,85 @@ func AddNetworkAttachment(name string, config map[string]interface{}) error {
 	return os.WriteFile(file, json, 0o644)
 }
 
+func GetAllNetworkAttachments() []map[string]interface{} {
+	cniConfigs := make([]map[string]interface{}, 0)
+	names := make([]string, 0)
+	err := filepath.Walk("/etc/cni/net.d", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		if !info.IsDir() {
+			names = append(names, strings.TrimSuffix(info.Name(), filepath.Ext(path)))
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, name := range names {
+		cniConfig, err := GetNetworkAttachment(name)
+		if err == nil {
+			cniConfigs = append(cniConfigs, cniConfig)
+		}
+	}
+	return cniConfigs
+}
+
+func GetNetworkAttachment(name string) (map[string]interface{}, error) {
+	var cniConfig map[string]interface{}
+	file := filepath.Join("/etc/cni/net.d", name+".conflist")
+	conf, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(conf, &cniConfig)
+	if err != nil {
+		return nil, fmt.Errorf("addDeviceIDInConfList: failed to unmarshal inBytes: %v", err)
+	}
+
+	return cniConfig, nil
+}
+
 func RemoveNetworkAttachment(name string) error {
 	file := filepath.Join("/etc/cni/net.d", name+".conflist")
 	return os.Remove(file)
+}
+
+func IsAllocated(pciAddress string) network.VFStatus {
+	for _, cniConfig := range GetAllNetworkAttachments() {
+		pList, ok := cniConfig["plugins"]
+		if !ok {
+			if _, ok := cniConfig["type"]; ok {
+				deviceID, ok := cniConfig["deviceID"].(string)
+				if ok && deviceID == pciAddress {
+					return network.VFStatus_USED
+				}
+			}
+			continue
+		}
+
+		pMap, ok := pList.([]interface{})
+		if !ok {
+			continue
+		}
+
+		for _, plugin := range pMap {
+			currentPlugin, ok := plugin.(map[string]interface{})
+			if !ok {
+				continue
+			}
+
+			deviceID, ok := currentPlugin["deviceID"].(string)
+			if ok && deviceID == pciAddress {
+				return network.VFStatus_USED
+			}
+
+		}
+
+	}
+
+	return network.VFStatus_FREE
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -23,7 +23,6 @@ package server
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	network "github.com/sriramy/vf-operator/pkg/api/v1/gen/network"
@@ -63,14 +62,12 @@ func (s *NetworkServiceServer) CreateResourceConfig(_ context.Context, c *networ
 
 	s.resources = append(s.resources, *r)
 	s.configure(r)
-	return s.getResource(r), nil
+	return s.buildResource(r), nil
 }
 
 func (s *NetworkServiceServer) GetResourceConfig(_ context.Context, id *network.ResourceName) (*network.ResourceConfig, error) {
-	for _, r := range s.resources {
-		if r.config.GetName() == id.GetName() {
-			return r.config, nil
-		}
+	if r := s.getResource(id.GetName()); r != nil {
+		return r.config, nil
 	}
 	return nil, status.Errorf(codes.NotFound, "resource id=%s not found", id.GetName())
 }
@@ -97,22 +94,30 @@ func (s *NetworkServiceServer) GetAllResourceConfigs(context.Context, *empty.Emp
 func (s *NetworkServiceServer) GetAllResources(context.Context, *empty.Empty) (*network.Resources, error) {
 	resources := make([]*network.Resource, 0)
 	for _, r := range s.resources {
-		resources = append(resources, s.getResource(&r))
+		resources = append(resources, s.buildResource(&r))
 	}
 
 	return &network.Resources{Resources: resources}, nil
 }
 
 func (s *NetworkServiceServer) GetResource(_ context.Context, id *network.ResourceName) (*network.Resource, error) {
-	for _, r := range s.resources {
-		if r.config.GetName() == id.GetName() {
-			return s.getResource(&r), nil
-		}
+	if r := s.getResource(id.GetName()); r != nil {
+		return s.buildResource(r), nil
 	}
+
 	return nil, status.Errorf(codes.NotFound, "resource id=%s not found", id.GetName())
 }
 
-func (s *NetworkServiceServer) getResource(r *resource) *network.Resource {
+func (s *NetworkServiceServer) getResource(resourceName string) *resource {
+	for _, r := range s.resources {
+		if r.config.GetName() == resourceName {
+			return &r
+		}
+	}
+	return nil
+}
+
+func (s *NetworkServiceServer) buildResource(r *resource) *network.Resource {
 	discoveredDevices := make([]string, 0)
 	devices := make([]*network.ResourceStatus, 0)
 	for _, dev := range r.provider.GetDevices() {
@@ -153,81 +158,94 @@ func (s *NetworkServiceServer) getResource(r *resource) *network.Resource {
 
 func (s *NetworkServiceServer) GetAllNetworkAttachments(context.Context, *empty.Empty) (*network.NetworkAttachments, error) {
 	nas := make([]*network.NetworkAttachment, 0)
-	for _, na := range GetAll() {
-		config, err := structpb.NewStruct(na.config)
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "network attachment config id=%s not valid", na.name)
+	for _, na := range GetAllNetworkAttachments() {
+		n, err := s.getNetworkAttachment(na)
+		if err == nil {
+			nas = append(nas, n)
 		}
-
-		nas = append(nas, &network.NetworkAttachment{
-			Name:         na.name,
-			ResourceName: na.resourceName,
-			Config:       config,
-		})
 	}
 
-	fmt.Printf("nas: %v", nas)
 	return &network.NetworkAttachments{Networkattachments: nas}, nil
 }
 
 func (s *NetworkServiceServer) CreateNetworkAttachment(_ context.Context, na *network.NetworkAttachment) (*empty.Empty, error) {
-	if Get(na.GetName()) != nil {
+	cniConfig, _ := GetNetworkAttachment(na.GetName())
+	if cniConfig != nil {
 		return nil, status.Errorf(codes.AlreadyExists,
 			"network attachment name=%s already exists", na.GetName())
 	}
 
-	for _, r := range s.resources {
-		if r.config.GetName() == na.GetResourceName() {
-			for _, dev := range r.provider.GetDevices() {
-				for _, vf := range r.provider.GetVFDevices(dev) {
-					if IsAllocated(vf.PCIAddress) == network.VFStatus_FREE {
-						naConfig, err := BuildNetworkAttachmentConfig(na.GetConfig().AsMap(), vf.PCIAddress)
-						if err != nil {
-							return nil, status.Errorf(codes.InvalidArgument, "Cannot add device info: %v", err)
-						}
-						Store(na.GetName(), na.GetResourceName(), naConfig, vf.PCIAddress)
-						err = AddNetworkAttachment(na.GetName(), naConfig)
-						if err != nil {
-							RemoveNetworkAttachment(na.GetName())
-							Erase(na.GetConfig().String())
-							return nil, status.Errorf(codes.InvalidArgument, "Cannot add network attachment: %v", err)
-						}
-						return new(empty.Empty), nil
+	naConfig, err := AddNameToNetworkAttachment(na.GetConfig().AsMap(), na.GetName(), na.GetResourceName())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Cannot add name info: %v", err)
+	}
+
+	if na.GetResourceName() == "" {
+		err = AddNetworkAttachment(na.GetName(), naConfig)
+		if err != nil {
+			RemoveNetworkAttachment(na.GetName())
+			return new(empty.Empty), status.Errorf(codes.InvalidArgument, "Cannot add network attachment: %v", err)
+		}
+		return new(empty.Empty), nil
+	}
+
+	if r := s.getResource(na.GetResourceName()); r != nil {
+		for _, dev := range r.provider.GetDevices() {
+			for _, vf := range r.provider.GetVFDevices(dev) {
+				if IsAllocated(vf.PCIAddress) == network.VFStatus_FREE {
+					naConfig, err = AddDeviceIDToNetworkAttachment(na.GetConfig().AsMap(), vf.PCIAddress)
+					if err != nil {
+						return nil, status.Errorf(codes.InvalidArgument, "Cannot add device info: %v", err)
 					}
+					err = AddNetworkAttachment(na.GetName(), naConfig)
+					if err != nil {
+						RemoveNetworkAttachment(na.GetName())
+						return nil, status.Errorf(codes.InvalidArgument, "Cannot add network attachment: %v", err)
+					}
+					return new(empty.Empty), nil
 				}
 			}
-
-			return nil, status.Errorf(codes.ResourceExhausted, "resource id=%s has no free VFs", na.GetResourceName())
 		}
+
+		return nil, status.Errorf(codes.ResourceExhausted, "resource id=%s has no free VFs", na.GetResourceName())
+
 	}
 
 	return nil, status.Errorf(codes.NotFound, "resource id=%s not found", na.GetResourceName())
 }
 
 func (s *NetworkServiceServer) GetNetworkAttachment(_ context.Context, id *network.NetworkAttachmentName) (*network.NetworkAttachment, error) {
-	if na := Get(id.GetName()); na != nil {
-		config, err := structpb.NewStruct(na.config)
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "network attachment config id=%s not valid", id.GetName())
-		}
-		return &network.NetworkAttachment{
-			Name:         na.name,
-			ResourceName: na.resourceName,
-			Config:       config,
-		}, nil
+	na, err := GetNetworkAttachment(id.GetName())
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "network attachment id=%s not found", id.GetName())
 	}
 
-	return nil, status.Errorf(codes.NotFound, "network attachment id=%s not found", id.GetName())
+	return s.getNetworkAttachment(na)
+}
+
+func (*NetworkServiceServer) getNetworkAttachment(na map[string]interface{}) (*network.NetworkAttachment, error) {
+	// obtain names from network attachment
+	name, _ := na["name"].(string)
+	resourceName, _ := na["resourceName"].(string)
+
+	config, err := structpb.NewStruct(na)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "network attachment config id=%s not valid", name)
+	}
+	return &network.NetworkAttachment{
+		Name:         name,
+		ResourceName: resourceName,
+		Config:       config,
+	}, nil
 }
 
 func (s *NetworkServiceServer) DeleteNetworkAttachment(_ context.Context, id *network.NetworkAttachmentName) (*empty.Empty, error) {
-	if na := Get(id.GetName()); na != nil {
-		RemoveNetworkAttachment(id.GetName())
-		Erase(id.GetName())
-		return new(empty.Empty), nil
+	err := RemoveNetworkAttachment(id.GetName())
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "network attachment id=%s not found", id.GetName())
 	}
 
-	return nil, status.Errorf(codes.NotFound, "network attachment id=%s not found", id.GetName())
+	return new(empty.Empty), nil
 }
 
 func NewNetworkService(c *network.ResourceConfigs) *NetworkServiceServer {
